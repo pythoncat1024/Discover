9b2054b64a7acf21599e62a0d148624ec92e95e4
chengguiquan <chengguibao@rayootech.com>
2019-04-19 09:30:39 +0800

    * kotlin app with multiDex and set no actionbar

--- app/src/main/java/com/pycat/application/activity/MainActivity.kt
+++ app/src/main/java/com/pycat/application/activity/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.pycat.application.activity
 
-import androidx.appcompat.app.AppCompatActivity
-
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import com.pycat.application.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        getSharedPreferences("abc", Context.MODE_PRIVATE)
+                .edit {
+                    putBoolean("ok", true)
+                }
     }
 }
--- app/src/main/java/com/pycat/application/base/KotlinApp.kt
+++ app/src/main/java/com/pycat/application/base/KotlinApp.kt
@@ -1,16 +1,24 @@
 package com.pycat.application.base
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import com.apkfuns.logutils.LogUtils
 import com.pycat.application.BuildConfig
 
-class KotlinApp : Application() {
+class KotlinApp : MultiDexApplication() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
 
     override fun onCreate() {
         super.onCreate()
 
         LogUtils.getLogConfig()
                 .configTagPrefix(BuildConfig.APPLICATION_ID);
+        LogUtils.e("onCreate")
     }
 
     override fun onTerminate() {
