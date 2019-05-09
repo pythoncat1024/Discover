88c23c93ab53a01b015f74afda06a2b9ea687aad
pythonCat <pythoncat@qq.com>
2019-05-09 17:33:08 +0800

    * 蛤

---
+++ discover/src/main/java/com/pycat/discover/pig/AkFragment.java
@@ -0,0 +1,34 @@
+package com.pycat.discover.pig;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pycat.discover.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AkFragment extends Fragment {
+
+
+    public AkFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ak, container, false);
+    }
+
+}
